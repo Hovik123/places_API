05a0b5832a896a1e3be1078339dd2de841dba4ab
GhValentina <ghvalentina7@gmail.com>
2017-07-13 02:27:25 +0400

    add "pickup" and "destination" objects

--- controller/order.js
+++ controller/order.js
@@ -10,38 +10,86 @@ distanceAPI.apiKey = API_KEY;
  */
 function getOrder(req, res) {
     const params = {
-        location: `${req.param('lat')},${req.param('long')}`,
+        location: `${req.param('lat')},${req.param('lng')}`,
         radius: 5000
     };
-    places.nearbySearch(params)
-        .then((data) => {
-            const places = JSON.parse(data.text).results,
-                place = places[getRandomNumber(places.length - 1)],
-            {lat, lng} = place.geometry.location;
-            return getDistance({
-                origin: params.location,
-                destination: `${lat},${lng}`,
-            }).then(item => {
-                const order = {
-                    order: {
-                        id: getRandomNumber(10000),
-                        location: {
-                            address: place.vicinity,
-                            lat: lat,
-                            long: lng
-                        },
-                        duration: (item.durationValue / 60).toFixed(2),
-                        destination: item.distanceValue
+    getPlacesByParams(params)
+        .then(data => {
+            let { item, place } = data,
+                { lat, lng } = place.geometry.location
+                    const order = {
+                        order: {
+                            id: getRandomNumber(1000),
+                            taxiType: `${getRandomNumber(4)}`,
+                            pickup: {
+                                address: place.vicinity,
+                                lat: lat,
+                                lng: lng,
+                                duration: (item.durationValue / 60).toFixed(2),
+                                distance: item.distanceValue
+                            }
+                        }
+                    };
+            res.send(order);
+        })
+        .catch((err) => res.send(err));
+};
+
+function getOrderViaDestination(req, res) {
+    const params = {
+        location: `${req.param('lat')},${req.param('lng')}`,
+        radius: 5000
+    };
+    getPlacesByParams(params)
+        .then(data => {
+            let { item, place } = data,
+                { lat, lng } = place.geometry.location,
+                params = {
+                    location: `${lat},${lng}`,
+                    radius: 5000
+                }
+            getPlacesByParams(params)
+                .then(result => {
+                    let destinationItem = result.item,
+                        destinationPlace = result.place,
+                        destination = {
+                            lat: destinationPlace.geometry.location.lat,
+                            lng: destinationPlace.geometry.location.lng,
+                            duration: destinationItem.durationValue,
+                            distance: destinationItem.distanceValue
+                        }
+                    const order = {
+                        order: {
+                            id: getRandomNumber(1000),
+                            taxiType: `${getRandomNumber(4)}`,
+                            pickup: {
+                                address: place.vicinity,
+                                lat: lat,
+                                lng: lng,
+                                duration: (item.durationValue / 60).toFixed(2),
+                                distance: item.distanceValue
+                            }
+                        }
+                    };
+                    if (isOdd()) {
+                        order.order.destination = {
+                            address: destinationItem.destination,
+                            lat: destination.lat,
+                            lng: destination.lng,
+                            duration: (destination.duration / 60).toFixed(2),
+                            distance: destination.distance
+                        }
                     }
-                };
-                res.send(order);
-            });
-        }).catch((err) => res.send(err));
+                    res.send(order);
+                })
+        })
+        .catch((err) => res.send(err));
 }
 
 function getRandomNumber(range) {
     return Math.floor((Math.random() * range) + 1);
 }
+
 function getDistance(params = {}) {
     return new Promise((resolve, reject) => {
         distanceAPI.get(params, function (err, data) {
@@ -51,6 +99,29 @@ function getDistance(params = {}) {
     })
 }
 
+function getPlacesByParams(params = {}) {
+    return new Promise((resolve, reject) => {
+        places.nearbySearch(params)
+            .then(data => {
+                const places = JSON.parse(data.text).results,
+                    place = places[getRandomNumber(places.length - 1)];
+                let { lat, lng } = place.geometry.location;
+                return getDistance({
+                    origin: params.location,
+                    destination: `${lat},${lng}`,
+                })
+                    .then((item) => {
+                        resolve({ item, place });
+                    });
+            })
+            .catch(err => reject(err));
+    });
+};
+function isOdd(){
+    return getRandomNumber(4) % 2 !== 0; 
+}
+
 module.exports = {
-    order: getOrder
+    order: getOrder,
+    getOrderViaDestination
 };
