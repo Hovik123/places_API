3e718fb72b92e0df1192e273774fa0b0485211a9
Valentina <ghvalentina7@gmail.com>
2017-08-23 18:16:24 +0400

    add get nearby places

--- routes/order.js
+++ routes/order.js
@@ -4,5 +4,6 @@ const orderCtrl  = require('../controller/order');
 
 router.route('/').get(orderCtrl.order);
 router.route('/v2').get(orderCtrl.getOrderViaDestination);
+router.route('/v1').get(orderCtrl.getPlaces);
 
 module.exports = router;
